7d8ef4884797340b25d78c3b27c5b9a741ccd055
ahmetson <ahmetson@zoho.com>
2021-09-16 10:32:25 +0700

    Add cors policy

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000;
 
+app.use(cors()); // <---- use cors middleware
 app.use(express.static('public'))
 
 app.get('/', (_req, res) => {
